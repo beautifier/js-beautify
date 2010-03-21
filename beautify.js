743da2a3384a680ecc6de506909b75ef05657553
Einar Lielmanis <einars@gmail.com>
2010-03-21 02:54:37 +0200

    Mention braces_on_own_line option

--- beautify.js
+++ beautify.js
@@ -17,13 +17,14 @@
     js_beautify(js_source_text, options);
 
   The options are:
-    indent_size (default 4) — indentation size,
-    indent_char (default space) — character to indent with,
+    indent_size (default 4)          — indentation size,
+    indent_char (default space)      — character to indent with,
     preserve_newlines (default true) — whether existing line breaks should be preserved,
-    indent_level (default 0)  — initial indentation level, you probably won't need this ever,
+    indent_level (default 0)         — initial indentation level, you probably won't need this ever,
 
     space_after_anon_function (default false) — if true, then space is added between "function ()"
             (jslint is happy about this); if false, then the common "function()" output is used.
+    braces_on_own_line (default false) - ANSI / Allman brace style, each opening/closing brace gets its own line.
 
     e.g
 
