59d7aee4d3117e412bfb99f4f8db370505c5f7e4
Nicolas Ferrero <nicolas@niko.sencha>
2010-10-14 04:10:55 +0800

    Add command line arguments support to qtscript version

--- qtscript/jsbeautify.cpp
+++ qtscript/jsbeautify.cpp
@@ -1,5 +1,6 @@
 /*
-    Copyright (C) 2010 Ariya Hidayat <ariya.hidayat@gmail.com>
+    Copyright (c) 2010 Nicolas Ferrero <ferrero.nicolas@gmail.com>
+    Copyright (c) 2010 Ariya Hidayat <ariya.hidayat@gmail.com>
     Copyright (c) 2009 Einar Lielmanis
 
     Permission is hereby granted, free of charge, to any person
@@ -33,7 +34,6 @@ static QString readFile(const QString &fileName)
     QFile file;
     file.setFileName(fileName);
     if (!file.open(QFile::ReadOnly)) {
-        std::cerr << "Can't open" << qPrintable(fileName) << std::endl;
         return QString();
     }
     QString content = file.readAll();
@@ -41,24 +41,79 @@ static QString readFile(const QString &fileName)
     return content;
 }
 
+static void usage(char *progname)
+{
+    std::cerr << "Usage: " << progname << " [options] source-file\n";
+    std::cerr << "[options]\n";
+    std::cerr << " --indent-size                or -s: Indentation size. (default 4)\n";
+    std::cerr << " --indent-char                or -c: Character to indent with. (default space)\n";
+    std::cerr << " --disable-preserve-newlines  or -d: Do not preserve existing line breaks.\n";
+    std::cerr << " --indent-level               or -l: Initial indentation level, you probably won't need this ever. (default 0)\n";
+    std::cerr << " --space-after-anon-function  or -f: Space is added between \"function ()\", otherwise the common \"function()\" output is used.\n";
+    std::cerr << " --braces-on-own-line         or -b: ANSI / Allman brace style, each opening/closing brace gets its own line.\n";
+    std::cerr << " --keep-array-indentation     or -k: Keep array indentation.\n";
+    std::cerr << " --help                       or -h: Prints this help statement.\n";
+
+}
+
 int main(int argc, char **argv)
 {
     QCoreApplication app(argc, argv);
-
-    if (argc < 2) {
-        std::cout << "Usage:" << std::endl << std::endl;
-        std::cout << "    jsbeautify source-file" << std::endl << std::endl;
-        return 0;
+    QScriptEngine engine;
+    QScriptValue options = engine.newObject();
+    QString source;
+    
+    for (int argpos = 1; argpos < app.argc(); ++argpos) {
+        if (app.argv()[argpos][0] != '-') {
+            source = readFile(QString::fromLocal8Bit(app.argv()[argpos]));
+            
+        } else if (strcmp(app.argv()[argpos], "--indent-size") == 0 ||
+              strcmp(app.argv()[argpos], "-s") == 0) {
+              options.setProperty("indent_size", app.argv()[argpos+1]);
+              
+        } else if (strcmp(app.argv()[argpos], "--indent-char") == 0 ||
+              strcmp(app.argv()[argpos], "-c") == 0) {
+              options.setProperty("indent_char", app.argv()[argpos+1]);
+              
+        } else if (strcmp(app.argv()[argpos], "--disable-preserve-newlines") == 0 ||
+              strcmp(app.argv()[argpos], "-d") == 0) {
+              options.setProperty("preserve_newlines", false);
+        
+        } else if (strcmp(app.argv()[argpos], "--indent-level") == 0 ||
+              strcmp(app.argv()[argpos], "-l") == 0) {
+              options.setProperty("indent_level", app.argv()[argpos+1]);
+        
+        } else if (strcmp(app.argv()[argpos], "--space-after-anon-function") == 0 ||
+              strcmp(app.argv()[argpos], "-f") == 0) {
+              options.setProperty("space_after_anon_function", true); 
+              
+        } else if (strcmp(app.argv()[argpos], "--braces-on-own-line") == 0 ||
+              strcmp(app.argv()[argpos], "-b") == 0) {
+              options.setProperty("braces_on_own_line", true);      
+              
+        } else if (strcmp(app.argv()[argpos], "--keep-array-indentation") == 0 ||
+              strcmp(app.argv()[argpos], "-k") == 0) {
+              options.setProperty("keep_array_indentation", true);   
+              
+        } else if (strcmp(app.argv()[argpos], "--help") == 0 ||
+              strcmp(app.argv()[argpos], "-h") == 0) {
+                usage(app.argv()[0]);
+                return -1;
+        }
     }
-
+    
+    if (source.isEmpty()) {
+        usage(app.argv()[0]);
+        return -1;
+    }
+    
     QString script = readFile(":/beautify.js");
-    QString source = readFile(QString::fromLocal8Bit(argv[1]));
-
-    if (!script.isEmpty() && !source.isEmpty()) {
-        QScriptEngine engine;
+    
+    if (!script.isEmpty()) {
         engine.evaluate(script);
         engine.globalObject().setProperty("source", QScriptValue(source));
-        QScriptValue result = engine.evaluate("js_beautify(source);");
+        engine.globalObject().setProperty("options", options);
+        QScriptValue result = engine.evaluate("js_beautify(source, options);");
         std::cout << qPrintable(result.toString()) << std::endl;
     }
 
