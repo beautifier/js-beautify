70d721a67e63c539c40a4b6e41c40b1571f28b5b
Einar Lielmanis <einars@gmail.com>
2010-12-09 08:40:15 +0200

    Update the failed test

--- tests/beautify-tests.js
+++ tests/beautify-tests.js
@@ -115,7 +115,7 @@ function run_beautifier_tests(test_obj)
     bt('a=~a', 'a = ~a');
     bt('a;/*comment*/b;', "a; /*comment*/\nb;");
     bt('a;/* comment */b;', "a; /* comment */\nb;");
-    test_fragment('a;/*\ncomment\n*/b;', "a;\n/*\n comment\n */\nb;"); // simple comments don't get reindented
+    test_fragment('a;/*\ncomment\n*/b;', "a;\n/*\ncomment\n*/\nb;"); // simple comments don't get touched at all
     bt('a;/**\n* javadoc\n*/b;', "a;\n/**\n * javadoc\n */\nb;");
     bt('if(a)break;', "if (a) break;");
     bt('if(a){break}', "if (a) {\n    break\n}");
