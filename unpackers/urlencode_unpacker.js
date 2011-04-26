6fe3fe64a9075f8d9382b2793d6bdd3dd0549358
Einar Lielmanis <einars@gmail.com>
2011-04-26 11:57:14 +0300

    bookmarklet_unpacker -> urlencode_unpacker
    Nudge the unpacker detection as well.

--- unpackers/urlencode_unpacker.js
+++ unpackers/urlencode_unpacker.js
@@ -4,23 +4,25 @@
 //
 // usage:
 //
-// if (EscapedBookmarklet.detect(some_string)) {
-//     var unpacked = EscapedBookmarklet.unpack(some_string);
+// if (Urlencoded.detect(some_string)) {
+//     var unpacked = Urlencoded.unpack(some_string);
 // }
 // 
 //
 
-var EscapedBookmarklet = {
+var Urlencoded = {
     detect: function (str) {
         // the fact that script doesn't contain any space, but has %20 instead
         // should be sufficient check for now.
-        if (str.indexOf('%20') != -1 && str.indexOf(' ') == -1) return true;
-        if (str.indexOf('%7B') != -1 && str.indexOf('{') == -1) return true;
+        if (str.indexOf(' ') == -1) {
+            if (str.indexOf('%20') != -1) return true;
+            if (str.replace(/[^%]+/g, '').length > 3) return true;
+        }
         return false;
     },
 
     unpack: function (str) {
-        if (EscapedBookmarklet.detect(str)) {
+        if (Urlencoded.detect(str)) {
             return unescape(str.replace(/\+/g, '%20'));
         }
         return str;
@@ -30,12 +32,13 @@ var EscapedBookmarklet = {
 
     run_tests: function (sanity_test) {
         var t = sanity_test || new SanityTest();
-        t.test_function(EscapedBookmarklet.detect, "EscapedBookmarklet.detect");
+        t.test_function(Urlencoded.detect, "Urlencoded.detect");
         t.expect('', false);
         t.expect('var a = b', false);
         t.expect('var%20a+=+b', true);
         t.expect('var%20a=b', true);
-        t.test_function(EscapedBookmarklet.unpack, 'EscapedBookmarklet.unpack');
+        t.expect('var%20%21%22', true);
+        t.test_function(Urlencoded.unpack, 'Urlencoded.unpack');
         t.expect('', '');
         t.expect('abcd', 'abcd');
         t.expect('var a = b', 'var a = b');
