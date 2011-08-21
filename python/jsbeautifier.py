d01d007c71a5e35dabad63b7419654591eedc6e9
Stefano Sanfilippo <a.little.coder@gmail.com>
2011-08-21 23:03:17 +0200

    Added automagical transform of \x-encoded strings (i.e. replacing readable characters obfuscated as \x codes with the equivalent)
    Conflicts:
    
    	python/jsbeautifier.py

--- python/jsbeautifier.py
+++ python/jsbeautifier.py
@@ -867,10 +867,7 @@ class Beautifier:
 
         # Try to replace \x-encoded characters with their readable equivalent,
         # if it is possible (e.g. '\x41\x42\x43\x01' becomes 'ABC\x01').
-        try:
-            token_text = token_text.encode().decode('unicode_escape')
-        except UnicodeError:
-            pass
+        token_text = token_text.decode('unicode_escape', 'ignore')
 
         self.append(token_text)
 
