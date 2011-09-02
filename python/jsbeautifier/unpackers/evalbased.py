8c5a1ad44fb4536472bfbb1ee1537f9213e9f382
Stefano Sanfilippo <a.little.coder@gmail.com>
2011-09-02 22:48:30 +0200

    Added decoder for eval() based code.

---
+++ python/jsbeautifier/unpackers/evalbased.py
@@ -0,0 +1,34 @@
+#
+# Unpacker for eval() based packers, a part of javascript beautifier
+# by Einar Lielmanis <einar@jsbeautifier.org>
+#
+#     written by Stefano Sanfilippo <a.little.coder@gmail.com>
+#
+# usage:
+#
+# if detect(some_string):
+#     unpacked = unpack(some_string)
+#
+
+PRIORITY = 3
+
+def detect(source):
+    # NOTE: AT THE MOMENT, IT IS DEACTIVATED FOR YOUR SECURITY.
+    # uncomment following line to restore it.
+    #return source.strip().lower().startswith('eval(function(')
+    return False
+
+def unpack(source):
+    return js('print %s;' % source[4:]) if detect(source) else source
+
+# In case of failure, we'll just return the original, without crashing on user.
+def js(script):
+    from subprocess import PIPE, Popen
+    try:
+        interpreter = Popen(['js'], stdin=PIPE, stdout=PIPE)
+    except OSError:
+        return script
+    result, errors = interpreter.communicate(script)
+    if interpreter.poll() or errors:
+        return script
+    return result
