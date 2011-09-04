bcf725bc77b3c70cf1816cb7bf7ca396f893e165
Stefano Sanfilippo <a.little.coder@gmail.com>
2011-09-04 22:18:30 +0200

    Added long description.

--- python/setup.py
+++ python/setup.py
@@ -5,6 +5,8 @@ from distutils.core import setup
 setup(name='jsbeautifier',
       version='1.0',
       description='JavaScript unobfuscator and beautifier.',
+      long_description=('Beautify, unpack or deobfuscate JavaScript. '
+                        'Handles popular online obfuscators.'),
       author='Einar Lielmanis et al.',
       author_email='einar@jsbeautifier.org',
       url='http://jsbeautifier.org',
