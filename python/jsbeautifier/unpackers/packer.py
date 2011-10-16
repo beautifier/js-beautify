47f6634a620acd271672816c1d9f8f63b3a46106
Stefano <stefano.k.sanfilippo+development@gmail.com>
2011-10-16 21:10:19 +0200

    Added support for base95 encoding. Rewritten unbaser as functor to have it handle more encodings (now, adding an encoding is a matter of adding a line to Unbaser.ALPHABET variable).

--- python/jsbeautifier/unpackers/packer.py
+++ python/jsbeautifier/unpackers/packer.py
@@ -30,8 +30,8 @@ def unpack(source):
         raise UnpackingError('Malformed p.a.c.k.e.r. symtab.')
 
     try:
-        unbase = UNBASERS[radix]
-    except KeyError:
+        unbase = Unbaser(radix)
+    except TypeError:
         raise UnpackingError('Unknown p.a.c.k.e.r. encoding.')
 
     def lookup(match):
@@ -67,18 +67,38 @@ def _replacestrings(source):
         return source[startpoint:]
     return source
 
-ALPHABET  = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
-BASE_DICT = dict((cipher, index) for index, cipher in enumerate(ALPHABET))
 
-def unbase62(string):
-    """Decodes a base62 value to an integer."""
-    ret = 0
-    for index, cipher in enumerate(string[::-1]):
-        ret += (62 ** index) * BASE_DICT[cipher]
-    return ret
+class Unbaser(object):
+    """Functor for a given base. Will efficiently convert
+    strings to natural numbers."""
+    ALPHABET  = {
+        62 : '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ',
+        95 : (' !"#$%&\'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+              '[\]^_`abcdefghijklmnopqrstuvwxyz{|}~')
+    }
 
-UNBASERS = {
-    62 : unbase62,
-    10 : int, # int() will implicitly convert base-10 strings
-    36 : lambda string: int(string, 36)
-}
+    def __init__(self, base):
+        self.base = base
+
+        # If base can be handled by int() builtin, let it do it for us
+        if 2 <= base <= 36:
+            self.unbase = lambda string: int(string, base)
+        else:
+            # Build conversion dictionary cache
+            try:
+                self.dictionary = dict((cipher, index) for
+                    index, cipher in enumerate(self.ALPHABET[base]))
+            except KeyError:
+                raise TypeError('Unsupported base encoding.')
+
+            self.unbase = self._dictunbaser
+
+    def __call__(self, string):
+        return self.unbase(string)
+
+    def _dictunbaser(self, string):
+        """Decodes a  value to an integer."""
+        ret = 0
+        for index, cipher in enumerate(string[::-1]):
+            ret += (self.base ** index) * self.dictionary[cipher]
+        return ret
