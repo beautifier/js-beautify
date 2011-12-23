2d172560f037199ee9bd916183caa4b39abe7128
nightwing <amirjanyan@gmail.com>
2011-12-23 11:52:41 +0400

    cleanup

--- beautify-css.js
+++ beautify-css.js
@@ -28,7 +28,7 @@
 */
 
 // http://www.w3.org/TR/CSS21/syndata.html#tokenization
-// http://www.w3.org/TR/css3-syntax/#SUBTOK-nl
+// http://www.w3.org/TR/css3-syntax/
 function css_beautify(source_text, options) {
     options = options || {};
     var indentSize = options.indent_size || 4;
@@ -40,7 +40,6 @@ function css_beautify(source_text, options) {
 
 
     // tokenizer
-    var spaceRe = /^[\t ]+$/;
     var whiteRe = /^\s+$/;
     var wordRe = /[\w$\-_]/;
 
@@ -52,56 +51,50 @@ function css_beautify(source_text, options) {
         return source_text.charAt(pos+1)
     }
     function eatString(comma) {
-        var start = pos
+        var start = pos;
         while(next()){
             if (ch=="\\"){
                 next();
-                next()
+                next();
             } else if (ch == comma) {
-                break
+                break;
             } else if (ch == "\n") {
-                break
+                break;
             }
         }
-        return source_text.substring(start, pos + 1)
-    };
-
-    function eatWord() {
-        while(next() && rx.test(ch));
-        return ch
-    }
-    function eatSpace() {
-        while(next() == ' ');
-        return ch
-    }
-    function eatWhile(rx) {
-        while(next() && rx.test(ch));
-        return ch
+        return source_text.substring(start, pos + 1);
     }
 
     function eatWhitespace() {
-        var start = pos
+        var start = pos;
         while (whiteRe.test(peek()))
-            pos++
-        return pos != start
+            pos++;
+        return pos != start;
+    }
+
+    function skipWhitespace() {
+        var start = pos;
+        do{
+        }while (whiteRe.test(next()))
+        return pos != start + 1;
     }
 
     function eatComment() {
         var start = pos;
-        next()
+        next();
         while (next()) {
             if (ch == "*" && peek() == "/") {
-                pos ++
+                pos ++;
                 break;
             }
         }
 
-        return source_text.substring(start, pos + 1)
+        return source_text.substring(start, pos + 1);
     }
 
 
     function lookBack(str, index) {
-        return output.slice(-str.length + (index||0), index).join("").toLowerCase() == str
+        return output.slice(-str.length + (index||0), index).join("").toLowerCase() == str;
     }
 
     // printer
@@ -109,90 +102,89 @@ function css_beautify(source_text, options) {
     var singleIndent = Array(indentSize + 1).join(indentCharacter);
     var indentLevel = 0;
     function indent() {
-        indentLevel++
-        indentString += singleIndent
+        indentLevel++;
+        indentString += singleIndent;
     }
     function outdent() {
-        indentLevel--
-        indentString = indentString.slice(0, -indentSize)
+        indentLevel--;
+        indentString = indentString.slice(0, -indentSize);
     }
 
     print = {}
     print["{"] = function(ch) {
-        print.singleSpace()
-        output.push(ch)
-        print.newLine()
+        print.singleSpace();
+        output.push(ch);
+        print.newLine();
     }
     print["}"] = function(ch) {
-        print.newLine()
-        output.push(ch)
-        print.newLine()
+        print.newLine();
+        output.push(ch);
+        print.newLine();
     }
 
     print.newLine = function(keepWhitespace) {
         if (!keepWhitespace)
             while (whiteRe.test(output[output.length - 1]))
-                output.pop()
+                output.pop();
 
         if (output.length)
-            output.push("\n", indentString)
+            output.push('\n', indentString);
         else
-            output.push(indentString)
+            output.push(indentString);
     }
     print.singleSpace = function() {
         if (output.length && !whiteRe.test(output[output.length - 1]))
-            output.push(" ")
+            output.push(' ');
     }
     var output = [];
     if (indentString)
-        output.push(indentString)
+        output.push(indentString);
     /*_____________________--------------------_____________________*/
 
     while(true) {
-        var afterSpace = eatWhitespace()
-        next()
+        var isAfterSpace = skipWhitespace();
 
         if (!ch)
             break;
 
         if (ch == '{') {
-            indent()
-            print["{"](ch)
+            indent();
+            print["{"](ch);
         } else if (ch == '}') {
-            outdent()
-            print["}"](ch)
+            outdent();
+            print["}"](ch);
         } else if (ch == '"' || ch == '\'') {
             output.push(eatString(ch))
         } else if (ch == ';') {
-            output.push(ch, "\n", indentString)
-        } else if (ch == '/' && peek() == "*") { // comment
-            print.newLine()
-            output.push(eatComment(), "\n", indentString)
-        } else if (ch == '(') { // uri
-            output.push(ch)
-            afterSpace = eatWhitespace()
+            output.push(ch, '\n', indentString);
+        } else if (ch == '/' && peek() == '*') { // comment
+            print.newLine();
+            output.push(eatComment(), "\n", indentString);
+        } else if (ch == '(') { // may be a url
+            output.push(ch);
+            eatWhitespace();
             if (lookBack("url", -1) && next()) {
                 if (ch != ')' && ch != '"' && ch != '\'')
-                    output.push(eatString(')'))
+                    output.push(eatString(')'));
                 else
-                    pos--
+                    pos--;
             }
         } else if (ch == ')') {
-            output.push(ch)
+            output.push(ch);
         } else if (ch == ',') {
-            eatWhitespace()
-            output.push(ch)
-            print.singleSpace()
+            eatWhitespace();
+            output.push(ch);
+            print.singleSpace();
         } else if (ch == ']') {
-            output.push(ch)
-        }  else if (ch == '[') {
-            eatWhitespace()
-            output.push(ch)
+            output.push(ch);
+        }  else if (ch == '[' || ch == '=') { // no whitespace before or after
+            eatWhitespace();
+            output.push(ch);
         } else {
-            if (afterSpace)
-                print.singleSpace()
+            if (isAfterSpace)
+                print.singleSpace();
 
-            output.push(ch)
+            output.push(ch);
         }
     }
 
