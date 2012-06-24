c0eb97ea0946655b7e6bae34e43835ee4312fb23
VittGam <github@vittgam.net>
2012-06-24 23:09:17 +0200

    Added tests for the decode_characters option in the Python test file.

--- python/jsbeautifier/tests/testjsbeautifier.py
+++ python/jsbeautifier/tests/testjsbeautifier.py
@@ -17,11 +17,22 @@ class TestJSBeautifier(unittest.TestCase):
         bt("'\\s'"); # == '\s' in the js source
         bt('"•"');
         bt('"—"');
-        #bt('"\x41\x42\x43\x01"', '"ABC\\x01"');
-        bt('"\u2022"', '"\u2022"');
+        bt('"\\x41\\x42\\x43\\x01"', '"\\x41\\x42\\x43\\x01"');
+        bt('"\\u2022"', '"\\u2022"');
         bt('a = /\s+/')
-        #bt('a = /\x41/','a = /A/')
-        bt('"\u2022";a = /\s+/;"\x41\x42\x43\x01".match(/\x41/);','"\u2022";\na = /\s+/;\n"\x41\x42\x43\x01".match(/\x41/);')
+        #bt('a = /\\x41/','a = /A/')
+        bt('"\\u2022";a = /\s+/;"\\x41\\x42\\x43\\x01".match(/\\x41/);','"\\u2022";\na = /\s+/;\n"\\x41\\x42\\x43\\x01".match(/\\x41/);')
+	bt('"\\x22\\x27",\'\\x22\\x27\',"\\x5c",\'\\x5c\',"\\xff and \\xzz"', '"\\x22\\x27", \'\\x22\\x27\', "\\x5c", \'\\x5c\', "\\xff and \\xzz"');
+
+        self.options.decode_characters = True
+
+        bt('"\\x41\\x42\\x43\\x01"', '"ABC\\x01"');
+        bt('"\\u2022"', '"\\u2022"');
+        bt('a = /\s+/')
+        bt('"\\u2022";a = /\s+/;"\\x41\\x42\\x43\\x01".match(/\\x41/);','"\\u2022";\na = /\s+/;\n"ABC\\x01".match(/\\x41/);')
+	bt('"\\x22\\x27",\'\\x22\\x27\',"\\x5c",\'\\x5c\',"\\xff and \\xzz"', '"\\"\'", \'"\\\'\', "\\\\", \'\\\\\', "\\xff and \\xzz"');
+
+        self.options.decode_characters = False
 
     def test_beautifier(self):
         test_fragment = self.decodesto
