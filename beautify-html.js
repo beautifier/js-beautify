132ca467a4a1d1506f3ceda6fbd3e3f6e1ecd536
Daniel Stockman <daniels@zillow.com>
2013-03-18 18:40:24 -0700

    Ensure html_beautify can access JS and CSS beautifiers when called from node.

--- beautify-html.js
+++ beautify-html.js
@@ -37,6 +37,11 @@
     });
 */
 
+if (typeof process !== 'undefined' && process.argv[0] === "node") {
+    var css_beautify = require('./beautify-css.js').css_beautify;
+    var js_beautify = require('./beautify.js').js_beautify;
+}
+
 function style_html(html_source, options) {
 //Wrapper function to invoke all the necessary constructors and deal with the output.
 
