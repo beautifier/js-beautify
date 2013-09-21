590e937dbf3c35ce105679af30d85d1c0d9b4027
Alexander Chernetz <achernetz@gmail.com>
2013-09-21 11:25:56 -0400

    Replace errno with os.errno to fix ImportError bug

--- python/jsbeautifier/__init__.py
+++ python/jsbeautifier/__init__.py
@@ -1392,7 +1392,7 @@ def mkdir_p(path):
     try:
         os.makedirs(path)
     except OSError as exc: # Python >2.5
-        if exc.errno == errno.EEXIST and os.path.isdir(path):
+        if exc.errno == os.errno.EEXIST and os.path.isdir(path):
             pass
         else: raise
 
