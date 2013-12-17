fa0220f4df637273a47cc2651f4bcd8b9627a733
Tomás Girardi <tgirardi@gmail.com>
2013-12-17 18:09:57 -0300

    Change --comma_first for --comma-first
    Use the dash instead of underscore for the name of the client argument
    related with comma first style (as suggested by @evocateur).

--- js/lib/cli.js
+++ js/lib/cli.js
@@ -93,7 +93,7 @@ var fs = require('fs'),
         "x": ["--unescape_strings"],
         "w": ["--wrap_line_length"],
         "X": ["--e4x"],
-        "F": ["--comma_first"],
+        "F": ["--comma-first"],
         // HTML-only
         "W": ["--max_char"], // obsolete since 1.3.5
         "U": ["--unformatted"],
@@ -217,7 +217,7 @@ function usage(err) {
             msg.push('  -x, --unescape-strings        Decode printable characters encoded in xNN notation');
             msg.push('  -w, --wrap-line-length        Wrap lines at next opportunity after N characters [0]');
             msg.push('  -X, --e4x                     Pass E4X xml literals through untouched');
-            msg.push('  -F, --comma_first             Use comma first format in variables declarations and objects');
+            msg.push('  -F, --comma-first             Use comma first format in variables declarations and objects');
             msg.push('  --good-stuff                  Warm the cockles of Crockford\'s heart');
             break;
         case "html":
