42824bd7b849fbd71eefbef6c256016427a516a4
johvik <john.viklund@gmail.com>
2013-12-30 12:41:17 +0100

    Preserve newlines in html related to issue #307
    For preserve_newlines and max_preserve_newlines false or 0 isn't accepted as configuration. This PR allows both those scenarios.

--- js/lib/beautify-html.js
+++ js/lib/beautify-html.js
@@ -105,8 +105,13 @@
         brace_style = options.brace_style || 'collapse';
         wrap_line_length =  parseInt(options.wrap_line_length, 10) === 0 ? 32786 : parseInt(options.wrap_line_length || 250, 10);
         unformatted = options.unformatted || ['a', 'span', 'bdo', 'em', 'strong', 'dfn', 'code', 'samp', 'kbd', 'var', 'cite', 'abbr', 'acronym', 'q', 'sub', 'sup', 'tt', 'i', 'b', 'big', 'small', 'u', 's', 'strike', 'font', 'ins', 'del', 'pre', 'address', 'dt', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6'];
-        preserve_newlines = options.preserve_newlines || true;
-        max_preserve_newlines = preserve_newlines ? parseInt(options.max_preserve_newlines || 32786, 10) : 0;
+        if (options.preserve_newlines !== false) {
+          preserve_newlines = true;
+          max_preserve_newlines = isNaN(parseInt(options.max_preserve_newlines, 10)) ? 32786 : parseInt(options.max_preserve_newlines, 10);
+        } else {
+          preserve_newlines = false;
+          max_preserve_newlines = 0;
+        }
         indent_handlebars = options.indent_handlebars || false;
 
         function Parser() {
