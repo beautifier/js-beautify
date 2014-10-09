7985e7d45d2039c12e3e3009a21cfcbdd3bfb231
Liam Newman <bitwiseman@gmail.com>
2014-10-08 18:17:54 -0700

    Move test data and template files

--- test/generate-tests.js
+++ test/generate-tests.js
@@ -4,11 +4,11 @@ var mustache = require('mustache');
 
 function generate_tests() {
     var test_data, template;
-    test_data = require('./data.js').test_data;
-    template = fs.readFileSync(__dirname + '/js-beautifier-node.mustache', {encoding: 'utf-8'});
+    test_data = require(__dirname + '/data/javascript.js').test_data;
+    template = fs.readFileSync(__dirname + '/template/node-javascript.mustache', {encoding: 'utf-8'});
     fs.writeFileSync(__dirname + '/../js/test/beautify-tests.js', mustache.render(template, test_data), {encoding: 'utf-8'});
 
-    template = fs.readFileSync(__dirname + '/js-beautifier-python.mustache', {encoding: 'utf-8'});
+    template = fs.readFileSync(__dirname + '/template/python-javascript.mustache', {encoding: 'utf-8'});
     fs.writeFileSync(__dirname + '/../python/jsbeautifier/tests/testjsbeautifier.py', mustache.render(template, test_data), {encoding: 'utf-8'});
 }
 
