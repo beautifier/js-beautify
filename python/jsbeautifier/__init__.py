c698c419f79a38307a28ff4764ae8fd6b912c02c
Chris Thomas <xiotox@gmail.com>
2015-03-08 13:32:46 -0400

    fixed python build errors

--- python/jsbeautifier/__init__.py
+++ python/jsbeautifier/__init__.py
@@ -616,7 +616,7 @@ class Beautifier:
             # TODO: option space_before_conditional
             self.output.space_before_token = True
 
-        elif current_token.text == '(' && self.last_type == 'TK_RESERVED' && self.flags.last_word == 'await')
+        elif current_token.text == '(' and self.last_type == 'TK_RESERVED' and self.flags.last_word == 'await':
             self.output.space_before_token = True
 
 
