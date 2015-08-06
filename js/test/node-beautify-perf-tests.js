ba072367f616018f84c304b9d8f9a77ce1c77144
mps <morgan.smith@ephox.com>
2015-08-06 16:53:53 +1000

    adding html beautify perf test

--- js/test/node-beautify-perf-tests.js
+++ js/test/node-beautify-perf-tests.js
@@ -13,6 +13,7 @@ function node_beautifier_tests() {
     console.log('Testing performance...');
     var data = fs.readFileSync(__dirname + '/../../test/underscore.js', 'utf8');
     var data_min = fs.readFileSync(__dirname + '/../../test/underscore-min.js', 'utf8');
+    var data_attr = fs.readFileSync(__dirname + '/../../../base.64.still.almost.breaking.js', 'utf8');
     var options = {
         wrap_line_length: 80
     };
@@ -20,7 +21,8 @@ function node_beautifier_tests() {
     //warm-up
     js_beautify(data, options);
     js_beautify(data_min, options);
-
+    html_beautify(data_attr, options);
+    
     var suite = new Benchmark.Suite;
 
     suite.add("js-beautify (underscore)", function() {
@@ -28,6 +30,9 @@ function node_beautifier_tests() {
     })
     .add("js-beautify (underscore-min)", function() {
         js_beautify(data_min, options);
+    })
+    .add("html-beautify (large-attribute)", function () {
+        html_beautify(data_attr, options);
     })
     // add listeners
     .on('cycle', function(event) {
