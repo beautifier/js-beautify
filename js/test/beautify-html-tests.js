53e98834370c8ce3e3ba717bf7e9ed91262e2884
Tom Marchant <thomas.marchant@johnlewis.co.uk>
2015-10-12 12:28:45 +0000

    Fixed root test file

--- js/test/beautify-html-tests.js
+++ js/test/beautify-html-tests.js
@@ -212,11 +212,6 @@ function run_html_tests(test_obj, Urlencoded, js_beautify, html_beautify, css_be
         test_fragment('<div class="{{#if thingIs \'value\'}}{{field}}{{/if}}"></div>');
         test_fragment('<div class=\'{{#if thingIs "value"}}{{field}}{{/if}}\'></div>');
         test_fragment('<div class=\'{{#if thingIs \'value\'}}{{field}}{{/if}}\'></div>');
-        test_fragment('<div class=\'{{#if thingIs \'value\'}}{{field}}{{else}}{{field}}{{/if}}\'></div>');
-        test_fragment(
-            '{{#if condition}}<div></div>{{else}}<div></div>{{/if}}',
-            '{{#if condition}}\n    <div></div>\n{{else}}\n    <div></div>\n{{/if}}');
-        test_fragment('{{#if condition}}<span></span>{{else}}<span></span>{{/if}}');
     
         // Handlebars Indenting On - (content = "{{! comment}}")
         opts.indent_handlebars = true;
@@ -383,6 +378,15 @@ function run_html_tests(test_obj, Urlencoded, js_beautify, html_beautify, css_be
         test_fragment('<div class=\'{{#if thingIs "value"}}{{! \n mult-line\ncomment  \n     with spacing\n}}{{/if}}\'></div>');
         test_fragment('<div class=\'{{#if thingIs \'value\'}}{{! \n mult-line\ncomment  \n     with spacing\n}}{{/if}}\'></div>');
     
+
+
+        // Handlebars Else tag indenting
+        opts.indent_handlebars = true;
+        test_fragment(
+            '{{#if test}}<div></div>{{else}}<div></div>{{/if}}',
+            '{{#if test}}\n    <div></div>\n{{else}}\n    <div></div>\n{{/if}}');
+        test_fragment('{{#if test}}<span></span>{{else}}<span></span>{{/if}}');
+
 
 
         // Unclosed html elements
