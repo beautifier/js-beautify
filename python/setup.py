814bec0e46a2dd4510970b6d6f8cc1dca0338a53
Matthias Urlichs <matthias@urlichs.de>
2015-12-13 23:50:56 +0100

    Update Python's setup.py
    * distutils no longer works correctly
    * fixed testing: use the py.test framework

--- python/setup.py
+++ python/setup.py
@@ -1,7 +1,25 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+import os,sys
+
+from setuptools import setup
 from jsbeautifier.__version__ import __version__
+
+from setuptools.command.test import test as TestCommand
+
+DIR='jsbeautifier/tests/'
+class PyTest(TestCommand):
+    user_options = [('pytest-args=', 'a', "Arguments to pass to py.test")]
+
+    def initialize_options(self):
+        TestCommand.initialize_options(self)
+        self.pytest_args = ['--assert=plain'] +[DIR+x for x in os.listdir(DIR) if x.endswith('.py') and x[0] not in '._']
+
+    def run_tests(self):
+        #import here, cause outside the eggs aren't loaded
+        import pytest
+        errno = pytest.main(self.pytest_args)
+        sys.exit(errno)
 
 setup(name='jsbeautifier',
       version=__version__,
@@ -16,4 +34,7 @@ setup(name='jsbeautifier',
                 'jsbeautifier.unpackers', 'jsbeautifier.unpackers.tests'],
       install_requires=["six>=1.6.1"],
       license='MIT',
+      test_suite='pytest.collector',
+      cmdclass = {'test': PyTest},
+
      )
