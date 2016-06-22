fd6bd185e873ea2f139cec07fb8d47adde5f261f
actionless <actionless.loveless@gmail.com>
2016-06-22 14:26:58 +0200

    fix(beautify-html: jinja): don't add precessing extra-space for jinja tag inside attribute

--- js/lib/beautify-html.js
+++ js/lib/beautify-html.js
@@ -465,10 +465,6 @@
                             var jinja_token = jinja_tokens[jinja_token_index];
                             if ((input_char + this.input.charAt(this.pos)) === jinja_token.start) {
                                 input_char += this.get_unformatted(jinja_token.end);
-                                if (content.length && content[content.length - 1] !== ' ' && content[content.length - 1] !== '<') {
-                                    input_char = ' ' + input_char;
-                                }
-                                space = true;
                             }
                         }
                     }
