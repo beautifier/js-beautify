d74c157e137e4b4eaa2443ebdc4b0ad6d300bc3e
Yu Luo <yuno2008@gmail.com>
2018-03-12 17:57:21 +0800

    issue-1156:fixed line break in mixins after background url
    Change-Id: I91fa77493eafb9de820eba802529dcf84ddd0170

--- js/lib/beautify-css.js
+++ js/lib/beautify-css.js
@@ -540,6 +540,7 @@ function Beautifier(source_text, options) {
                             print_string(eatString(')'));
                         } else {
                             pos--;
+                            parenLevel++;
                         }
                     }
                 } else {
