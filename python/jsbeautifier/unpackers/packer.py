11008f1926ab60dc12d22f835496b140494cd0db
swan46 <eleonora46@gmx.net>
2018-05-03 11:01:15 +0200

    Update packer.py
    added case, that radix is [] (default), and not numeric

--- python/jsbeautifier/unpackers/packer.py
+++ python/jsbeautifier/unpackers/packer.py
@@ -63,18 +63,22 @@ def unpack(source):
 
 def _filterargs(source):
     """Juice from a source file the four args needed by decoder."""
-    juicers = [ (r"}\('(.*)', *(\d+), *(\d+), *'(.*)'\.split\('\|'\), *(\d+), *(.*)\)\)"),
-                (r"}\('(.*)', *(\d+), *(\d+), *'(.*)'\.split\('\|'\)"),
+    juicers = [ (r"}\('(.*)', *(\d+|\[\]), *(\d+), *'(.*)'\.split\('\|'\), *(\d+), *(.*)\)\)"),
+                (r"}\('(.*)', *(\d+|\[\]), *(\d+), *'(.*)'\.split\('\|'\)"),
               ]
     for juicer in juicers:
         args = re.search(juicer, source, re.DOTALL)
         if args:
             a = args.groups()
+            if a[1] == "[]"  :
+               a = list(a)
+               a[1] = 62
+               a = tuple(a)
             try:
                 return a[0], a[3].split('|'), int(a[1]), int(a[2])
             except ValueError:
                 raise UnpackingError('Corrupted p.a.c.k.e.r. data.')
-
+                
     # could not find a satisfying regex
     raise UnpackingError('Could not make sense of p.a.c.k.e.r data (unexpected code structure)')
 
