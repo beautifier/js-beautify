e3eaf2284cdb55e271e2ee4d08ed3fb2255971b4
Liam Newman <bitwiseman@gmail.com>
2018-07-07 15:55:09 -0700

    Update build and release

--- tools/release-all.sh
+++ tools/release-all.sh
@@ -12,10 +12,10 @@ esac
 
 release_python()
 {
+    cd $SCRIPT_DIR/../python
     git clean -xfd || exit 1
     echo "__version__ = '$NEW_VERSION'" > python/jsbeautifier/__version__.py
-    git commit -am "Python $NEW_VERSION" || exit 1
-    cd python
+    git commit -am "Python $NEW_VERSION"
     # python setup.py register -r pypi
     python setup.py sdist || exit 1
     python -m twine upload dist/* || exit 1
@@ -24,31 +24,33 @@ release_python()
 
 release_node()
 {
-      git clean -xfd || exit 1
-      ./build js || exit 1
-      npm version $NEW_VERSION
-      unset NPM_TAG
-      if [[ $NEW_VERSION =~ .*(rc|beta).* ]]; then
-        NPM_TAG='--tag next'
-      fi
-      npm publish . $NPM_TAG
-      git push
-      git push --tags
+    cd $SCRIPT_DIR/..
+    git clean -xfd || exit 1
+    ./build js || exit 1
+    npm version $NEW_VERSION
+    unset NPM_TAG
+    if [[ $NEW_VERSION =~ .*(rc|beta).* ]]; then
+    NPM_TAG='--tag next'
+    fi
+    npm publish . $NPM_TAG
+    git push
+    git push --tags
 }
 
 release_web()
 {
-      local ORIGINAL_BRANCH
-      ORIGINAL_BRANCH=$(git branch | grep '[*] .*' | awk '{print $2}')
-      git clean -xfd || exit 1
-      git fetch || exit 1
-      git checkout -B gh-pages origin/gh-pages || exit 1
-      git merge origin/master || exit 1
-      ./build js || exit 1
-      git add -f js/lib/ || exit 1
-      git commit -m "Built files for $NEW_VERSION"
-      git push || exit 1
-      git checkout $ORIGINAL_BRANCH
+    cd $SCRIPT_DIR/..
+    local ORIGINAL_BRANCH
+    ORIGINAL_BRANCH=$(git branch | grep '[*] .*' | awk '{print $2}')
+    git clean -xfd || exit 1
+    git fetch || exit 1
+    git checkout -B gh-pages origin/gh-pages || exit 1
+    git merge origin/master || exit 1
+    ./build js || exit 1
+    git add -f js/lib/ || exit 1
+    git commit -m "Built files for $NEW_VERSION"
+    git push || exit 1
+    git checkout $ORIGINAL_BRANCH
 }
 
 sedi() {
@@ -84,7 +86,7 @@ main()
     git clean -xfd
 
     update_readme_versions
-    (release_python)
+    release_python
     release_node
     release_web
 }
