54f8e464b4b375640710955d9f1898f6cd5220e2
Liam Newman <bitwiseman@gmail.com>
2018-07-10 00:15:54 -0700

    Add first unit test for input scanner

---
+++ js/test/core/inputscanner.js
@@ -0,0 +1,10 @@
+var assert = require('assert');
+var InputScanner = require('../../src/core/inputscanner').InputScanner;
+
+describe('IndexScanner', function() {
+  describe('new', function() {
+    it('should return empty scanner when input is not present', function() {
+      assert.equal(new InputScanner().hasNext(), false);
+    });
+  });
+});
