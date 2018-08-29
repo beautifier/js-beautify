4807865f0258155a5afbbaad19bc171c41b43613
Liam Newman <bitwiseman@gmail.com>
2018-08-28 17:31:50 -0700

    Run more perf tests and on larger files

--- python/test-perf-jsbeautifier.py
+++ python/test-perf-jsbeautifier.py
@@ -17,10 +17,13 @@ def beautifier_test_underscore():
 def beautifier_test_underscore_min():
     jsbeautifier.beautify(data_min, options)
 
+def beautifier_test_github_min():
+    jsbeautifier.beautify(github_min, options)
+
 
 def report_perf(fn):
     import timeit
-    iter = 50
+    iter = 5
     time = timeit.timeit(
         fn +
         "()",
@@ -37,12 +40,17 @@ if __name__ == '__main__':
         dirname, "../", "test/resources/underscore.js")
     underscore_min_file = os.path.join(
         dirname, "../", "test/resources/underscore-min.js")
+    github_min_file = os.path.join(
+        dirname, "../", "test/resources/github-min.js")
     data = copy.copy(''.join(open(underscore_file).readlines()))
     data_min = copy.copy(''.join(open(underscore_min_file).readlines()))
+    github_min = copy.copy(''.join(open( github_min_file).readlines()))
 
     # warm up
     beautifier_test_underscore()
     beautifier_test_underscore_min()
+    beautifier_test_github_min()
 
     report_perf("beautifier_test_underscore")
     report_perf("beautifier_test_underscore_min")
+    report_perf("beautifier_test_github_min")
