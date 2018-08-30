6307b283687cf8b430ab52fc66fb56d3d6e3f90c
Liam Newman <bitwiseman@gmail.com>
2018-08-30 12:13:35 -0700

    Update to use release branch

--- tools/release-all.sh
+++ tools/release-all.sh
@@ -13,45 +13,34 @@ esac
 release_python()
 {
     cd $SCRIPT_DIR/..
+    git checkout -B release origin/release
     git clean -xfd || exit 1
-    echo "__version__ = '$NEW_VERSION'" > python/jsbeautifier/__version__.py
-    git commit -am "Python $NEW_VERSION"
     cd python
     # python setup.py register -r pypi
     python setup.py sdist || exit 1
     python -m twine upload dist/* || exit 1
-    git push
 }
 
 release_node()
 {
     cd $SCRIPT_DIR/..
+    git checkout -B release origin/release
     git clean -xfd || exit 1
-    make js || exit 1
-    npm version $NEW_VERSION
     unset NPM_TAG
     if [[ $NEW_VERSION =~ .*(rc|beta).* ]]; then
     NPM_TAG='--tag next'
     fi
     npm publish . $NPM_TAG || exit 1
-    git push
-    git push --tags
 }
 
 release_web()
 {
     cd $SCRIPT_DIR/..
-    local ORIGINAL_BRANCH
-    ORIGINAL_BRANCH=$(git branch | grep '[*] .*' | awk '{print $2}')
     git clean -xfd || exit 1
     git fetch || exit 1
     git checkout -B gh-pages origin/gh-pages || exit 1
-    git merge origin/master --no-edit || exit 1
-    make js || exit 1
-    git add -f js/lib/ || exit 1
-    git commit -m "Built files for $NEW_VERSION"
-    git push || exit 1
-    git checkout $ORIGINAL_BRANCH
+    git merge origin/release --no-edit || exit 1
+    git checkout master
 }
 
 sedi() {
@@ -64,15 +53,37 @@ sedi() {
     fi
 }
 
-update_readme_versions()
+update_versions()
 {
+    git fetch --all || exit 1
+    git checkout master || exit 1
+    git reset --hard origin/master || exit 1
     git clean -xfd || exit 1
+    npm version --no-git-tag-version $NEW_VERSION
+
     #sedi -E 's@(cdn.rawgit.+beautify/v)[^/]+@\1'$NEW_VERSION'@' README.md
     sedi -E 's@(cdnjs.cloudflare.+beautify/)[^/]+@\1'$NEW_VERSION'@' README.md
     sedi -E 's/\((README\.md:.js-beautify@).+\)/(\1'$NEW_VERSION')/' README.md
-    git add README.md
-    git commit -m "Bump version numbers in README.md"
+
+    echo "__version__ = '$NEW_VERSION'" > python/jsbeautifier/__version__.py
+    git add .
+    git commit -am "Bump version numbers for $NEW_VERSION"
     git push
+}
+
+update_release_branch()
+{
+    git reset --hard
+    git clean -xfd
+    git checkout -B release origin/release
+    git merge origin/master --no-edit || exit 1
+
+    make js || exit 1
+    git add -f js/lib/ || exit 1
+    git commit -m "Release: $NEW_VERSION"
+    npm version --allow-same-version --no-git-tag-version  $NEW_VERSION
+    git push || exit 1
+    git push --tags
 }
 
 main()
@@ -82,11 +93,9 @@ main()
     local NEW_VERSION=$1
     NEW_VERSION=$1
 
-    git checkout master
-    git reset --hard
-    git clean -xfd
+    update_versions
+    update_release_branch
 
-    update_readme_versions
     release_python
     release_node
     release_web
