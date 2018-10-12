d63816437daad516ba26cf95578236cd5f130c42
Liam Newman <bitwiseman@gmail.com>
2018-10-11 23:58:23 -0700

    Finish translation

--- python/jsbeautifier/javascript/beautifier.py
+++ python/jsbeautifier/javascript/beautifier.py
@@ -55,6 +55,7 @@ class BeautifierFlags:
         self.in_case_statement = False
         self.case_body = False
         self.indentation_level = 0
+        self.alignment = 0
         self.line_indent_level = 0
         self.start_line_index = 0
         self.ternary_depth = 0
@@ -324,7 +325,8 @@ class Beautifier:
                     self._flags.mode) and current_token.newlines:
                 line.push(current_token.whitespace_before)
                 self._output.space_before_token = False
-            elif self._output.set_indent(self._flags.indentation_level):
+            elif self._output.set_indent(self._flags.indentation_level,
+                    self._flags.alignment):
                 self._flags.line_indent_level = self._flags.indentation_level
 
     def print_token(self, current_token, s=None):
@@ -1230,7 +1232,7 @@ class Beautifier:
                            or l.strip() == '' for l in lines)
 
             if javadoc:
-                this._flags.alignment = 1
+                self._flags.alignment = 1
 
             for line in lines:
                 self.print_newline(preserve_statement_flags=True)
@@ -1244,8 +1246,10 @@ class Beautifier:
                     # normal comments output raw
                     self._output.add_token(line)
 
-            this._flags.alignment = 0
+            self._flags.alignment = 0
 
+        # for comments on their own line or  more than one line,
+        # make sure there's a new line after
         self.print_newline(preserve_statement_flags=preserve_statement_flags)
 
     def handle_comment(self, current_token, preserve_statement_flags):
