fa0705414d1b83c41db6aa04f8597fe550bdaa7b
Liam Newman <bitwiseman@gmail.com>
2018-10-11 20:36:48 -0700

    Use alignment in javascript beautifier

--- js/src/core/options.js
+++ js/src/core/options.js
@@ -49,6 +49,15 @@ function Options(options, merge_child_field) {
   this.indent_with_tabs = this._get_boolean('indent_with_tabs', this.indent_char === '\t');
   if (this.indent_with_tabs) {
     this.indent_char = '\t';
+
+    // indent_size behavior changed in 1.9.0.
+    // It used to be that indent_size would be set to 1 for indent_with_tabs.
+    // That is no longer needed and actually doesn't make sense - why not use spaces.
+    // Further, it might produce unexpeded behavior - tabs being used for single-column alignment.
+    // So, when indent_with_tabs is true and indent_size is 1, reset indent_size to 4.
+    if (this.indent_size === 1) {
+      this.indent_size = 4;
+    }
   }
 
   // Backwards compat with 1.3.x
