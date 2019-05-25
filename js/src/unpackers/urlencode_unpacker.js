9d9d2523514d788d479daaa67dd4f0109afbbc60
Liam Newman <bitwiseman@gmail.com>
2019-05-24 18:03:44 -0700

    2019

--- js/src/unpackers/urlencode_unpacker.js
+++ js/src/unpackers/urlencode_unpacker.js
@@ -5,7 +5,7 @@
 
   The MIT License (MIT)
 
-  Copyright (c) 2007-2018 Einar Lielmanis, Liam Newman, and contributors.
+  Copyright (c) 2007-2019 Einar Lielmanis, Liam Newman, and contributors.
 
   Permission is hereby granted, free of charge, to any person
   obtaining a copy of this software and associated documentation files
--- python/jsbeautifier/__init__.py
+++ python/jsbeautifier/__init__.py
@@ -16,7 +16,7 @@ from jsbeautifier.javascript.beautifier import Beautifier
 #
 # The MIT License (MIT)
 
-# Copyright (c) 2007-2018 Einar Lielmanis, Liam Newman, and contributors.
+# Copyright (c) 2007-2019 Einar Lielmanis, Liam Newman, and contributors.
 
 # Permission is hereby granted, free of charge, to any person
 # obtaining a copy of this software and associated documentation files
