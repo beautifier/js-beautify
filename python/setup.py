4d164580054900aeefbe65b591eca39a0f3b1bcd
Jorengarenar <jorengarenar@protonmail.com>
2020-01-17 21:20:17 +0100

    Merged setup-css.py into setup.py

--- python/setup.py
+++ python/setup.py
@@ -51,3 +51,25 @@ setup(name='jsbeautifier',
       cmdclass={'test': PyTest},
 
       )
+
+setup(name='cssbeautifier',
+      version=__version__,
+      description='CSS unobfuscator and beautifier.',
+      long_description=('Beautify, unpack or deobfuscate CSS'),
+      author='Liam Newman, Einar Lielmanis, et al.',
+      author_email='team@beautifier.io',
+      url='https://beautifier.io',
+      entry_points={
+          'console_scripts': [
+              'css-beautify = cssbeautifier:main'
+          ]
+      },
+      packages=['cssbeautifier',
+                'cssbeautifier.tests', 'cssbeautifier.tests.generated',
+                'cssbeautifier.css'],
+      install_requires=["jsbeautifier>=__version__",
+                        "six>=1.13.0",
+                        "editorconfig>=0.12.2"],
+      license='MIT',
+
+      )
