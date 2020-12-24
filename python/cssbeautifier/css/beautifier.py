73064d880b16d9990773d901cb1c3261c31400fe
Juan Medina <jfml_97@live.com>
2020-12-24 12:26:29 -0600

    Refactor code fix whitespace and indentation

--- python/cssbeautifier/css/beautifier.py
+++ python/cssbeautifier/css/beautifier.py
@@ -143,9 +143,12 @@ class Beautifier:
         while whitespaceChar.search(self._input.peek() or "") is not None:
             self._ch = self._input.next()
             if allowAtLeastOneNewLine and self._ch == "\n":
-                if newline_count == 0 or newline_count < self._options.max_preserve_newlines:
+                if (
+                    newline_count == 0 or
+                    newline_count < self._options.max_preserve_newlines
+                ):
                     newline_count += 1
-                    self._output.add_new_line(True)          
+                    self._output.add_new_line(True)
         return result
 
     # Nested pseudo-class if we are insideRule
