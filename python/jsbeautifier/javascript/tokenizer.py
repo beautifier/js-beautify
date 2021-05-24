3e678a41ff6071750da65273bba3030109f0cd6b
Rheidner <rheidner.achiles@gmail.com>
2021-05-24 11:44:45 -0300

    Issue beautify-web#1934 modify number regex to allow bigint octals, binarys and hexadecimals.

--- python/jsbeautifier/javascript/tokenizer.py
+++ python/jsbeautifier/javascript/tokenizer.py
@@ -62,7 +62,7 @@ TOKEN = TokenTypes()
 dot_pattern = re.compile(r"[^\d\.]")
 
 number_pattern = re.compile(
-    r"0[xX][0123456789abcdefABCDEF_]*|0[oO][01234567_]*|0[bB][01_]*|\d[\d_]*n|(?:\.\d[\d_]*|\d[\d_]*\.?[\d_]*)(?:[eE][+-]?[\d_]+)?"
+    r"0[xX][0123456789abcdefABCDEF_]*n?|0[oO][01234567_]*n?|0[bB][01_]*n?|\d[\d_]*n|(?:\.\d[\d_]*|\d[\d_]*\.?[\d_]*)(?:[eE][+-]?[\d_]+)?"
 )
 digit = re.compile(r"[0-9]")
 
