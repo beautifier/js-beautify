9fc5745a7b4e37479b5a71c0997580edc68c7914
Reshma R <reshma.ramaprasad@gmail.com>
2021-06-22 09:27:50 +0530

    change test output to multi line format

--- test/data/javascript/tests.js
+++ test/data/javascript/tests.js
@@ -3446,7 +3446,18 @@ exports.test_data = {
         {
           comment: "Issue #1683 - switch-case wrong indentation",
           input: 'switch (x) { case 0: if (y == z) { a(); } else { b(); } case 1: c(); }',
-          output: 'switch (x) {\n    case 0:\n        if (y == z) {\n            a();\n        } else {\n            b();\n        }\n    case 1:\n        c();\n}'
+          output: [
+            'switch (x) {',
+            '    case 0:',
+            '        if (y == z) {',
+            '            a();',
+            '        } else {',
+            '            b();',
+            '        }',
+            '    case 1:',
+            '        c();',
+            '}'
+          ]
         },
         {
           comment: "Issue 485 - ensure function declarations behave the same in arrays as elsewhere",
