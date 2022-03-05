ab205a40ec6027509627d455b4ca6bf50c619e87
Muhammad Hammad <mhnaeem.dev@gmail.com>
2022-03-05 17:12:48 -0330

    feature: allow mac users to use command+enter for submit button

--- web/onload.js
+++ web/onload.js
@@ -39,10 +39,14 @@ $(function() {
 
 
   $(window).bind('keydown', function(e) {
-    if (e.ctrlKey && e.keyCode === 13) {
+    if ((e.ctrlKey || e.metaKey) && e.keyCode === 13) {
       beautify();
     }
   });
+
+  if(typeof window.navigator !== "undefined" && typeof window.navigator.platform === "string" && window.navigator.platform.includes("Mac")) {
+    $(".submit em").text("(cmd-enter)");
+  }
 
   $('.submit').click(beautify);
   $('select').change(beautify);
