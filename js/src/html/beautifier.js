7fe98948dc6974d7cdb628ff33a03fef7fdb6a8f
avento <avento@umich.edu>
2022-04-18 17:03:07 -0400

    reran cli

--- js/src/html/beautifier.js
+++ js/src/html/beautifier.js
@@ -613,13 +613,13 @@ var TagOpenParserToken = function(parent, raw_token) {
       // handle "{{#> myPartial}}" or "{{~#> myPartial}}"
       if ((raw_token.text.startsWith('{{#>') || raw_token.text.startsWith('{{~#>')) && this.tag_check[0] === '>') {
         if (this.tag_check === '>' && raw_token.next !== null) {
-            this.tag_check = raw_token.next.text.split(' ')[0];
+          this.tag_check = raw_token.next.text.split(' ')[0];
         } else {
-             this.tag_check = raw_token.text.split('>')[1];
+          this.tag_check = raw_token.text.split('>')[1];
         }
       }
     }
-    
+
     this.tag_check = this.tag_check.toLowerCase();
 
     if (raw_token.type === TOKEN.COMMENT) {
