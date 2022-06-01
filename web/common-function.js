b80f8487345c04c37b1cbb4eaabb432f443e0b27
Ilan Cohen <ilan.cohen@moovit.com>
2022-06-01 15:35:31 +0300

    fix for compilation

--- web/common-function.js
+++ web/common-function.js
@@ -1,5 +1,5 @@
 /*jshint strict:false, node:false */
-/*exported run_tests, read_settings_from_cookie, beautify, submitIssue, copyText, selectAll, clearAll, changeToFileContent*/
+/*exported run_tests, read_settings_from_cookie, beautify, submitIssue, copyText, selectAll, clearAll, changeToFileContent, checkForDarkTheme*/
 var the = {
   use_codemirror: !window.location.href.match(/without-codemirror/),
   beautifier_file: window.location.href.match(/debug/) ? 'beautifier' : './beautifier.min',
