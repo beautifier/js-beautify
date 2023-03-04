9f8086613b9d2cf83f0e89d1a863dc4e4dcdf0cb
Rouak-stha <rounakshrestha1153@gmail.com>
2023-03-04 16:34:19 +0545

    Fixed #2133 Theme Toggle on without_codemirror Mode

--- web/onload.js
+++ web/onload.js
@@ -16,7 +16,7 @@ $(function() {
     the.editor = CodeMirror.fromTextArea(textArea, {
       lineNumbers: true
     });
-    setPreferredColorScheme();
+
     set_editor_mode();
     the.editor.focus();
 
@@ -36,7 +36,8 @@ $(function() {
       }
     });
   }
-
+  
+  setPreferredColorScheme();
 
   $(window).bind('keydown', function(e) {
     if ((e.ctrlKey || e.metaKey) && e.keyCode === 13) {
