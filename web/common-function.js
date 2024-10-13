b06b4dc4dcdc98a05d1b0de22990572f622cf194
Bankn8II©$A <102619282+barionleg@users.noreply.github.com>
2024-10-13 13:40:26 +0200

    Update common-function.js
    added to 392 row to end: function toggleDarkMode() { .....

--- web/common-function.js
+++ web/common-function.js
@@ -389,3 +389,12 @@ function switchTheme(themeToggleEvent) {
     $('.logo').children('img').attr("src", "web/banner-light.svg");
   }
 }
+function toggleDarkMode() {
+  let isDark = document.body.classList.toggle('dark-mode');
+  localStorage.setItem('darkMode', isDark ? 'enabled' : 'disabled');
+} 
+// On page load
+document.addEventListener('DOMContentLoaded', (event) => {
+  if (localStorage.getItem('darkMode') === 'enabled') {
+    document.body.classList.add('dark-mode'); } 
+});
